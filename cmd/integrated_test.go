536668358523ae03fe73fd9e6041b570fd295216
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-06-01 20:20:43 -0300

    [FEATURE]: Configured the envs, docker, and main to build the server

---
+++ cmd/integrated_test.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"Rate-Limiter-Challenge-GoExpertPostGrad/internal/limiter"
+	"Rate-Limiter-Challenge-GoExpertPostGrad/internal/middleware"
+	"Rate-Limiter-Challenge-GoExpertPostGrad/pkg/persistence"
+	"context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+var redisClient *persistence.RedisClient
+
+func TestMain(m *testing.M) {
+	// Set up the Redis client
+	redisClient = persistence.NewRedisClient("localhost:6379", "", 0)
+	// Run tests
+	code := m.Run()
+	os.Exit(code)
+}
+
+func cleanRedis() {
+	redisClient.FlushAll(context.Background())
+}
+
+func TestValidTokenWithinLimit(t *testing.T) {
+	cleanRedis()
+	rateLimiter := limiter.NewLimiter(redisClient, 2, 10, 5, []string{"token1", "token2", "token3"})
+	mw := middleware.NewRateLimiterMiddleware(rateLimiter)
+
+	for i := 0; i < 10; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		assert.NoError(t, err)
+		req.Header.Set("API_KEY", "token1")
+		req.RemoteAddr = "192.168.0.1:12345"
+
+		rr := httptest.NewRecorder()
+		handler := mw.Limit(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+	}
+}
+
+func TestValidTokenExceedingLimit(t *testing.T) {
+	cleanRedis()
+	rateLimiter := limiter.NewLimiter(redisClient, 2, 10, 5, []string{"token1", "token2", "token3"})
+	mw := middleware.NewRateLimiterMiddleware(rateLimiter)
+
+	for i := 0; i < 11; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		assert.NoError(t, err)
+		req.Header.Set("API_KEY", "token1")
+		req.RemoteAddr = "192.168.0.1:12345"
+
+		rr := httptest.NewRecorder()
+		handler := mw.Limit(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler.ServeHTTP(rr, req)
+
+		if i < 10 {
+			assert.Equal(t, http.StatusOK, rr.Code)
+		} else {
+			assert.Equal(t, http.StatusTooManyRequests, rr.Code)
+		}
+	}
+}
+
+func TestInvalidTokenWithinLimit(t *testing.T) {
+	cleanRedis()
+	rateLimiter := limiter.NewLimiter(redisClient, 2, 10, 5, []string{"token1", "token2", "token3"})
+	mw := middleware.NewRateLimiterMiddleware(rateLimiter)
+
+	for i := 0; i < 2; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		assert.NoError(t, err)
+		req.Header.Set("API_KEY", "invalid-token")
+		req.RemoteAddr = "192.168.0.2:12345"
+
+		rr := httptest.NewRecorder()
+		handler := mw.Limit(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+	}
+}
+
+func TestInvalidTokenExceedingLimit(t *testing.T) {
+	cleanRedis()
+	rateLimiter := limiter.NewLimiter(redisClient, 2, 10, 5, []string{"token1", "token2", "token3"})
+	mw := middleware.NewRateLimiterMiddleware(rateLimiter)
+
+	for i := 0; i < 3; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		assert.NoError(t, err)
+		req.Header.Set("API_KEY", "invalid-token")
+		req.RemoteAddr = "192.168.0.2:12345"
+
+		rr := httptest.NewRecorder()
+		handler := mw.Limit(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler.ServeHTTP(rr, req)
+
+		if i < 2 {
+			assert.Equal(t, http.StatusOK, rr.Code)
+		} else {
+			assert.Equal(t, http.StatusTooManyRequests, rr.Code)
+		}
+	}
+}
+
+func TestNoTokenWithinLimit(t *testing.T) {
+	cleanRedis()
+	rateLimiter := limiter.NewLimiter(redisClient, 2, 10, 5, []string{"token1", "token2", "token3"})
+	mw := middleware.NewRateLimiterMiddleware(rateLimiter)
+
+	for i := 0; i < 2; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		assert.NoError(t, err)
+		req.RemoteAddr = "192.168.0.3:12345"
+
+		rr := httptest.NewRecorder()
+		handler := mw.Limit(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+	}
+}
+
+func TestNoTokenExceedingLimit(t *testing.T) {
+	cleanRedis()
+	rateLimiter := limiter.NewLimiter(redisClient, 2, 10, 5, []string{"token1", "token2", "token3"})
+	mw := middleware.NewRateLimiterMiddleware(rateLimiter)
+
+	for i := 0; i < 3; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		assert.NoError(t, err)
+		req.RemoteAddr = "192.168.0.3:12345"
+
+		rr := httptest.NewRecorder()
+		handler := mw.Limit(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler.ServeHTTP(rr, req)
+
+		if i < 2 {
+			assert.Equal(t, http.StatusOK, rr.Code)
+		} else {
+			assert.Equal(t, http.StatusTooManyRequests, rr.Code)
+		}
+	}
+}
---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"Rate-Limiter-Challenge-GoExpertPostGrad/internal/configs"
+	"Rate-Limiter-Challenge-GoExpertPostGrad/internal/limiter"
+	"Rate-Limiter-Challenge-GoExpertPostGrad/internal/router"
+	"Rate-Limiter-Challenge-GoExpertPostGrad/pkg/persistence"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config, err := configs.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	redisClient := persistence.NewRedisClient(config.RedisAddr, config.RedisPassword, config.RedisDB)
+	rateLimiter := limiter.NewLimiter(redisClient, config.RateLimitByIP, config.RateLimiteByToken, config.Ttl, config.Tokens)
+
+	r := router.NewRouter(rateLimiter)
+
+	log.Printf("Server starting on port %d", config.ServerPort)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.ServerPort), r)
+}
